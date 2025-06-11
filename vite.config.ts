ebd9d6e719d49c3213f129b8f321d5bd64f7a3e5
yeshwant <yeshwantreddyanugu@gmail.com>
2025-06-11 20:03:02 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/zovoa/",
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
